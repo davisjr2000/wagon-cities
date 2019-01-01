eee27d4851b02a1c88013ebcfdd4cda801d7daa0
Davis JR <davisjr2000@hotmail.com>
2019-01-01 13:05:29 -0200

    First commit of 2019! Add Reducer

--- src/containers/city_list.jsx
+++ src/containers/city_list.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import City from './city'
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { setCities } from '../actions';
 
-const CityList = (props) => {
-  const renderList = () => {
-    return props.cities.map((city, index) => {
+function mapStateToProps(state) {
+   return {
+    cities: state.cities
+   };
+}
+
+function mapDispatchToProps(dispatch){
+ return bindActionCreators(
+  { setCities: setCities },
+  dispatch
+  );
+}
+
+class CityList extends Component {
+
+  componentWillMount() {
+    this.props.setCities();
+ }
+
+  render(){
+    return this.props.cities.map((city, index) => {
       return(
         <City
         name={city.name}
@@ -14,12 +35,6 @@ const CityList = (props) => {
       )
     })
   }
-
-  return (
-    <div className="city-list">
-      {renderList()}
-    </div>
-    )
 }
 
-export default CityList;
+export default connect(mapStateToProps, mapDispatchToProps)(CityList);
